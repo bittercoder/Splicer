221ea2638fc80dfe9edd35248b09391ff95038a0
Alex Henderson <alex@devdefined.com>
2006-09-26 03:22:39 +0000

    Included Apache license boilerplate in applicable source files, dependent libraries and text file with full apache license now included as one of the solution items.

--- Splicer/Utils/FilterGraphTools.cs
+++ Splicer/Utils/FilterGraphTools.cs
@@ -1,9 +1,19 @@
-/****************************************************************************
-While the underlying libraries are covered by LGPL, this sample is released 
-as public domain.  It is distributed in the hope that it will be useful, but 
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-or FITNESS FOR A PARTICULAR PURPOSE.  
-*****************************************************************************/
+// Copyright 2004-2006 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// this class is largely derived from a public domain class that is released
+// as part of the DirectShow.Net samples.
 
 using System;
 using System.Collections;
